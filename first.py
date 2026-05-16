2a3c16e0dfcce112999dd6eec2e3a6dd808cacab
poddaramit <amitpoddar7304@gmail.com>
2026-05-16 00:14:24 -0700

    updated code

--- first.py
+++ first.py
@@ -1,13 +1,13 @@
-"""print in python"""
+#print in python
 print("Hello World")
 
-"""comments in python"""
+#comments in python
 """
 my name is Amit
 
 """
 
-"""assign multiple variables"""
+#assign multiple variables
 fruit = ["apple", "banana", "cherry"]
 x, y, z = fruit
 print(x)
